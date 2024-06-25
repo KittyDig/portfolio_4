8320ad946f4de13c168592bea3674018849d3c8c
KittyDig <kittydignam@gmail.com>
2024-06-25 11:06:13 +0000

    added comments to views and models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# create your models here.
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# custom User model inheriting from AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+# profile model to add to the User model with more information
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)  # bio, which is optional
+    # profile picture, which is optional
+    image = models.ImageField(upload_to='profile_pics', blank=True)
+
+# post model to store blog posts or similar content
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)  # title for the post
+    content = models.TextField()  # content of the post
+    created_at = models.DateTimeField(auto_now_add=True)  # timestamp for post
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .models import Post
+
+# for handling user registration
+
+
+def register(request):
+    if request.method == 'POST':  # if the form has been submitted
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # saving the user to the database
+            return redirect('login')
+    else:  # if its a GET request
+        form = UserCreationForm()
+    # renders the registration template with the form
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required  # ensures the user is logged in to view this page
+def home(request):
+    posts = Post.objects.all()  # retrieves all posts from the database
+    # renders the home template with all of the posts
+    return render(request, 'home.html', {'posts': posts})
