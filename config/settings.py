ba8f7007bbb75cfc816db0e9f1687cb9baa6e5b9
Kitty Dignam <kittydignam@gmail.com>
2024-06-27 14:20:53 +0000

    edited template format in settings

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +65,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
