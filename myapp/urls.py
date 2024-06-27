835f7f67c2d9e64c8947f308efe8514e736d72a8
KittyDig <kittydignam@gmail.com>
2024-06-27 11:47:05 +0000

    Added logout.html basic file

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,9 @@ from . import views  # importing my views module
 urlpatterns = [
     path('', views.home, name='home'),  # home page
     path('register/', views.register, name='register'),  # register page
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'),
+         name='login'),  # login page
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'),
+         name='logout'),  # logout page
+
 ]
