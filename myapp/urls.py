fc802dd7e534076526be627614f103530749e481
KittyDig <kittydignam@gmail.com>
2024-06-27 11:57:16 +0000

    Added auth_views import to urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views  # importing my views module
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),  # home page
