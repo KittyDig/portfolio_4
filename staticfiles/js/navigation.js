871c32c45aede5abe423d04e74664cbde0236aaf
Kitty Dignam <kittydignam@gmail.com>
2024-06-28 14:15:46 +0000

    fixed js file to load properly

--- staticfiles/js/navigation.js
+++ staticfiles/js/navigation.js
@@ -1,11 +1,12 @@
 // JavaScript for navigating between pages
+document.addEventListener('DOMContentLoaded', function() {
+    // function to navigate from the home page to post page
+    document.getElementById('goToPostPage').addEventListener('click', function() {
+        window.location.href = '{% url "posts" %}';
+    });
 
-// function to navigate from the home page to post page
-document.getElementById('goToPostPage').addEventListener('click', function() {
-    window.location.href = '{% url "posts" %}';
-});
-
-// function to navigate from post page to home page
-document.getElementById('goToHomePage').addEventListener('click', function() {
-    window.location.href = '{% url "home" %}';
+    // function to navigate from post page to home page
+    document.getElementById('goToHomePage').addEventListener('click', function() {
+        window.location.href = '{% url "home" %}';
+    });
 });
