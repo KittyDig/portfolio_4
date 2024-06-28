3cb2d370ba975476d9b36695e0b70f1b267a6eea
Kitty Dignam <kittydignam@gmail.com>
2024-06-28 14:39:10 +0000

    Created forms.py

---
+++ myapp/forms.py
@@ -0,0 +1,9 @@
+# myapp/forms.py
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
