aba91f5b266fc9115ca82f256126fecd44bc28c9
Kitty Dignam <kittydignam@gmail.com>
2024-06-30 18:06:48 +0000

    custom user model for registering users

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,8 +2,15 @@
 
 from django import forms
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email')
