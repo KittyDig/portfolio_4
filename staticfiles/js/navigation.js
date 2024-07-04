f129285129b968702f43f59bbf590e629d176b7f
Kitty Dignam <kittydignam@gmail.com>
2024-07-04 22:11:46 +0000

    fixed like model

--- staticfiles/js/navigation.js
+++ staticfiles/js/navigation.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var goToHomePageButton = document.getElementById('goToHomePage');
     if (goToHomePageButton) {
         goToHomePageButton.addEventListener('click', function() {
-            window.location.href = homeUrl;
+            window.location.href = home;
         });
     }
 });
